5f7f81e394f770bf49d89753d7b8f0fba4f241f1
Antonio Martín <ajms66@gmail.com>
2025-06-05 14:18:57 +0200

    5-6-2025 14:20

--- 09_matriculaciones/src/app.module.ts
+++ 09_matriculaciones/src/app.module.ts
@@ -5,19 +5,32 @@ import { Curso } from './model/Curso';
 import { Alumno } from './model/Alumno';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Matricula } from './model/Matricula';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3307,
-    username: 'nestuser',
-    password: 'nestpass',
-    database: 'formacion',
-    entities: [Curso,Alumno,Matricula],
-    synchronize: false,
-  }), TypeOrmModule.forFeature([Curso,Alumno,Matricula])],
+  imports: [ConfigModule.forRoot({
+         isGlobal: true, // hace que esté disponible en toda la app
+        }),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          
+          type: 'mysql',
+          host: config.get('URL_BD'),
+          port: parseInt(config.get('PORT_BD')),
+          username: config.get('USER_BD'),
+          password: config.get('PASSWORD'),
+          database: 'formacion',
+          entities: [Curso,Alumno,Matricula],
+          synchronize: false, 
+        })
+        
+        ,
+      })
+  
+        , TypeOrmModule.forFeature([Curso,Alumno,Matricula])],
   controllers: [MatriculacionController],
   providers: [MatriculacionService],
 })
