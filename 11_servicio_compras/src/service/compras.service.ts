797094d2de7abe0b868c5670a277a6218717ad4f
Antonio Martín <ajms66@gmail.com>
2025-06-17 09:33:22 +0200

    17-6-2025 9:30

--- 11_servicio_compras/src/service/compras.service.ts
+++ 11_servicio_compras/src/service/compras.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import axios from 'axios';
 import { PedidoDto } from 'src/dtos/PedidoDto';
 import { ProductoDto } from 'src/dtos/ProductoDto';
 
 
 @Injectable()
-export class ComprasService {
+export class ComprasService implements OnModuleInit{
   urlBase="http://localhost:3000/tienda";
+  urlLogin="http://localhost:3000/auth/login";
+  token:string;
+  async onModuleInit(){
+    //obtenemos el token al principio
+    this.token=(await axios.post(this.urlLogin,{username:"usuario1",password:"usuario1"})).data.valorToken;
+  }
+
 
   async buscarProductos(min:number,max:number):Promise<ProductoDto[]>{
-    const response:any=await axios.get(`${this.urlBase}/productos`);
+    const response:any=await axios.get(`${this.urlBase}/productos`,{
+      headers:{
+        Authorization: `Bearer ${this.token}`
+      }
+    });
     const jsonFiltrado:any=response.data.filter(p=>p.precioUnitario>=min&&p.precioUnitario<=max);
+    
     const productos:ProductoDto[]=jsonFiltrado.map(
       p=>{
         let disponibilidad:string="";
