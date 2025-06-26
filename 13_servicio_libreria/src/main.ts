8dfab84d06d37d24c7b2ddd4ae6e8fd71d720f92
Antonio Martín <ajms66@gmail.com>
2025-06-26 11:50:29 +0200

    26-6-2025 11:50

--- 13_servicio_libreria/src/main.ts
+++ 13_servicio_libreria/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
     origin: 'http://localhost:4200',
     credentials: true
   }); 
+  
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
